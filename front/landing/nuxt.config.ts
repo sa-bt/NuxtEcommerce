9592152b6259ca90e68e687fadbf74890e7ecd6f
Pourya <bakhshian2020@gmail.com>
2023-03-22 09:17:07 +0330

    add bootstrap-icons to project and add header

--- front/landing/nuxt.config.ts
+++ front/landing/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: "rtl",
+      },
+    },
+  },
   css: ["~/assets/css/main.css"],
 });
