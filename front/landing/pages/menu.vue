de814a799ab322efac0601fa9ac7fd5941f45c1f
Pourya <bakhshian2020@gmail.com>
2023-04-09 23:52:46 +0330

    create pagination for products in menu page

--- front/landing/pages/menu.vue
+++ front/landing/pages/menu.vue
@@ -1,84 +1,157 @@
 <template>
-    <!-- food section -->
-    <section class="food_section layout_padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 col-lg-3">
-                    <div>
-                        <label class="form-label">جستجو</label>
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="نام محصول ..."
-                                aria-label="Recipient's username" aria-describedby="basic-addon2">
-                            <a href="#" class="input-group-text" id="basic-addon2">
-                                <i class="bi bi-search"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="filter-list">
-                        <div class="form-label">
-                            دسته بندی
-                        </div>
-                        <ul>
-                            <li v-for="category in categories.data" :key="category.id" class="my-2 cursor-pointer filter-list-active">{{category.name}}</li>
-                        </ul>
-                    </div>
-                    <hr>
-                    <div>
-                        <label class="form-label">مرتب سازی</label>
-                        <div class="form-check my-2">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                            <label class="form-check-label cursor-pointer" for="flexRadioDefault1">
-                                بیشترین قیمت
-                            </label>
-                        </div>
-                        <div class="form-check my-2">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
-                                checked>
-                            <label class="form-check-label cursor-pointer" for="flexRadioDefault2">
-                                کمترین قیمت
-                            </label>
-                        </div>
-                        <div class="form-check my-2">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"
-                                checked>
-                            <label class="form-check-label cursor-pointer" for="flexRadioDefault3">
-                                پرفروش ترین
-                            </label>
-                        </div>
-                        <div class="form-check my-2">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"
-                                checked>
-                            <label class="form-check-label cursor-pointer" for="flexRadioDefault4">
-                                با تخفیف
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-lg-9">
-                    <div class="row gx-3">
-                        <div class="col-sm-6 col-lg-4" v-for="product in data.data.products" :key="product.id">
-                        <ProductCard :product="product"/>
-                        </div>
-                    </div>
-                    <nav class="d-flex justify-content-center mt-5">
-                        <ul class="pagination">
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        </ul>
-                    </nav>
-                </div>
+  <!-- food section -->
+  <section class="food_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-lg-3">
+          <div>
+            <label class="form-label">جستجو</label>
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="نام محصول ..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <a href="#" class="input-group-text" id="basic-addon2">
+                <i class="bi bi-search"></i>
+              </a>
             </div>
+          </div>
+          <hr />
+          <div class="filter-list">
+            <div class="form-label">دسته بندی</div>
+            <ul>
+              <li
+                v-for="category in categories.data"
+                :key="category.id"
+                class="my-2 cursor-pointer filter-list-active"
+              >
+                {{ category.name }}
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div>
+            <label class="form-label">مرتب سازی</label>
+            <div class="form-check my-2">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+              />
+              <label
+                class="form-check-label cursor-pointer"
+                for="flexRadioDefault1"
+              >
+                بیشترین قیمت
+              </label>
+            </div>
+            <div class="form-check my-2">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+                checked
+              />
+              <label
+                class="form-check-label cursor-pointer"
+                for="flexRadioDefault2"
+              >
+                کمترین قیمت
+              </label>
+            </div>
+            <div class="form-check my-2">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault3"
+                checked
+              />
+              <label
+                class="form-check-label cursor-pointer"
+                for="flexRadioDefault3"
+              >
+                پرفروش ترین
+              </label>
+            </div>
+            <div class="form-check my-2">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault4"
+                checked
+              />
+              <label
+                class="form-check-label cursor-pointer"
+                for="flexRadioDefault4"
+              >
+                با تخفیف
+              </label>
+            </div>
+          </div>
         </div>
-    </section>
-    <!-- end food section -->
+        <div class="col-sm-12 col-lg-9">
+
+            <div v-if="pending" class="d-flex justify-content-center align-items-center h-100">
+            <div class="spinner-border"></div>
+            </div>
+
+            <template v-else><div class="row gx-3">
+            <div
+              class="col-sm-6 col-lg-4"
+              v-for="product in data.data.products"
+              :key="product.id"
+            >
+              <ProductCard :product="product" />
+            </div>
+          </div>
+          <nav class="d-flex justify-content-center mt-5">
+            <ul class="pagination">
+              <li
+                v-for="(link, index) in data.data.meta.links.slice(1, -1)"
+                :key="index"
+                :class="{ active: link.active }"
+                class="page-item"
+              >
+                <button
+                  class="page-link"
+                  @click="handleFilter( link.label )"
+                >
+                  {{ link.label }}
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </template>
+          
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end food section -->
 </template>
 <script setup>
 
-const {public:{apiBase}}=useRuntimeConfig();
+const query = ref({});
+const {
+  public: { apiBase },
+} = useRuntimeConfig();
 
-const {data : categories}= await useFetch(`${apiBase}/categories`)
+const { data: categories } = await useFetch(`${apiBase}/categories`);
 
-const {data }= await useFetch(`${apiBase}/menu`)
+const { data, refresh, pending } = await useFetch(`${apiBase}/menu`, {
+  query: query,
+});
+
+function handleFilter(number) {
+  query.value = { page: number };
+
+  refresh();
+}
 </script>
