417503348f26232a2a971b178cf9b1122d73023f
Pourya <bakhshian2020@gmail.com>
2023-04-14 23:54:21 +0330

    create login page

---
+++ front/landing/pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+     <!-- login section -->
+     <section class="auth_section book_section">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-md-4 offset-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form_container">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="cellphone" class="form-label">شماره موبایل</label>
+                                        <input type="text" class="form-control" id="cellphone">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-auth">ورود</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end login section -->
+</template>
