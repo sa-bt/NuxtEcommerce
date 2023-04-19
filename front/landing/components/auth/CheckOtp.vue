af76f99f9bbed566eadf10736c6eb3069716f3a9
Pourya <bakhshian2020@gmail.com>
2023-04-20 02:16:59 +0330

    set token in front and fetch user information

--- front/landing/components/auth/CheckOtp.vue
+++ front/landing/components/auth/CheckOtp.vue
@@ -1,45 +1,37 @@
 <template>
-    
-    <div class="card-body">
-              <div class="form_container">
-                <div v-if="errors.length > 0" class="alert alert-danger">
-                  <ul class="mb-0">
-                    <li v-for="(error, index) in errors" :key="index">
-                      {{ error }}
-                    </li>
-                  </ul>
-                </div>
-                <form @submit.prevent="checkOtp">
-                  <div class="mb-3">
-                    <label for="otp" class="form-label"
-                      >کد اعتبارسنجی</label
-                    >
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="otp"
-                      v-model="otp"
-                    />
-                  </div>
-                  <button type="submit" class="btn btn-primary btn-auth">
-                    تایید
-                    <div
-                      v-if="loading"
-                      class="spinner-border spinner-border-sm ms-2"
-                    ></div>
-                  </button>
-                </form>
-              </div>
-            </div>
+  <div class="card-body">
+    <div class="form_container">
+      <div v-if="errors.length > 0" class="alert alert-danger">
+        <ul class="mb-0">
+          <li v-for="(error, index) in errors" :key="index">
+            {{ error }}
+          </li>
+        </ul>
+      </div>
+      <form @submit.prevent="checkOtp">
+        <div class="mb-3">
+          <label for="otp" class="form-label">کد اعتبارسنجی</label>
+          <input type="text" class="form-control" id="otp" v-model="otp" />
+        </div>
+        <button type="submit" class="btn btn-primary btn-auth">
+          تایید
+          <div
+            v-if="loading"
+            class="spinner-border spinner-border-sm ms-2"
+          ></div>
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 <script setup>
-
 import { useToast } from "vue-toastification";
 
 const otp = ref("");
 const toast = useToast();
 const errors = ref([]);
 const loading = ref(false);
+const { authUser } = useAuth();
 
 async function checkOtp() {
   console.log(otp.value);
@@ -59,7 +51,8 @@ async function checkOtp() {
       method: "POST",
       body: { otp: otp.value },
     });
-    toast.success('با موفقیت وارد شدید.');
+    toast.success("با موفقیت وارد شدید.");
+    authUser.value = data;
   } catch (error) {
     errors.value = Object.values(error.data.data.message).flat();
 
@@ -68,5 +61,4 @@ async function checkOtp() {
     loading.value = false;
   }
 }
-
 </script>
