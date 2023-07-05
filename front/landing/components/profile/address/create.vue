0253c42a91d065916dd945d12ca42389ca2b0b46
sa-bt <bakhshian2020@gmail.com>
2023-07-05 20:15:04 +0330

    create address component

---
+++ front/landing/components/profile/address/create.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    ایجاد آدرس جدید
+  </button>
+  <div class="collapse mt-3" id="collapseExample">
+    <div class="card card-body">
+      <div class="row g-4">
+        <div class="col col-md-6">
+          <label class="form-label">عنوان</label>
+          <input type="text" class="form-control">
+        </div>
+        <div class="col col-md-6">
+          <label class="form-label">شماره تماس</label>
+          <input type="text" class="form-control">
+        </div>
+        <div class="col col-md-6">
+          <label class="form-label">کد پستی</label>
+          <input type="text" class="form-control">
+        </div>
+        <div class="col col-md-6">
+          <label class="form-label">استان</label>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>تهران</option>
+            <option value="1">اصفهان</option>
+            <option value="2">فارس</option>
+            <option value="3">یزد</option>
+          </select>
+        </div>
+        <div class="col col-md-6">
+          <label class="form-label">شهر</label>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>تهران</option>
+            <option value="1">اصفهان</option>
+            <option value="2">شیراز</option>
+            <option value="3">یزد</option>
+          </select>
+        </div>
+        <div class="col col-md-12">
+          <label class="form-label">آدرس</label>
+          <textarea type="text" rows="5" class="form-control"></textarea>
+        </div>
+      </div>
+      <div>
+        <button class="btn btn-primary mt-4">ایجاد</button>
+      </div>
+    </div>
+  </div>
+  <hr>
+</template>
+
+<style scoped>
+
+</style>
